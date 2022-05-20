3e9f3ca07d2165a91b66f887c8c71d5e3eb24a60
Nestor Eduardo Ribero Vargas <nestor_05ribero@hotmail.com>
2022-05-19 19:24:40 -0500

    Update

--- card.js
+++ card.js
@@ -1,19 +1,35 @@
 
 var perrito = document.getElementById("perrito");
 
-perrito.addEventListener("mouseenter",()=>{
+perrito.addEventListener("mouseenter", () => {
     perrito.classList.remove("imgPeq");
     perrito.classList.add("imgGra");
 })
 
-perrito.addEventListener("mouseleave",()=>{
+perrito.addEventListener("mouseleave", () => {
     perrito.classList.add("imgPeq");
     perrito.classList.remove("imgGra");
 })
 
-var btnReset= document.getElementById("btnReset");
+var btnReset = document.getElementById("btnReset");
 
-btnReset.addEventListener("click",()=>{
-    var password= document.getElementById("password");
-    password.value="";
+btnReset.addEventListener("click", () => {
+    var password = document.getElementById("password");
+    password.value = "";
+})
+
+
+var welcome = document.getElementById("welcome");
+
+welcome.addEventListener("click", () => {
+    perrito.classList.remove("imgPeq");
+    perrito.classList.add("imgGra");
+    perrito.classList.add("hide");
+})
+
+welcome.addEventListener("dblclick", () => {
+    perrito.classList.remove("imgGra");
+    perrito.classList.add("imgPeq");
+    perrito.classList.remove("hide");
+
 })

eed429813bc90920393af9019319442a12449fb5
Nestor Eduardo Ribero Vargas <nestor_05ribero@hotmail.com>
2022-05-26 19:50:39 -0500

    Update videoTest.js

--- videoTest.js
+++ videoTest.js
@@ -4,9 +4,21 @@ var btnPlay = document.getElementById("btnPlay");
 
 videoPerrito.play();
 videoPerrito.pause();
-videoPerrito.currentTime=0;
+videoPerrito.currentTime = 0;
 
+var isPlaying = false;
 
-btnPlay.addEventListener("click",()=>{
+btnPlay.addEventListener("click", () => {
     videoPerrito.play();
+})
+
+videoPerrito.addEventListener("click", () => {
+    if (!isPlaying) {
+        videoPerrito.play();
+        isPlaying=!isPlaying;
+    }
+    else if (isPlaying) {
+        videoPerrito.pause();
+        isPlaying=!isPlaying;
+    }
 })
